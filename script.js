331c6e2c33790425738705b049ffb175f185b213
NGCMan <nintendogamecubeman@gmail.com>
2019-05-17 16:42:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,31 +6,85 @@ $("#counter").text(counter);
 
 // When button is clicked
 $("#add1").click(function(){
-  //Add 1 to counter
+  //Counter
   counter = counter.add(1);
 	// Display total
 	$("#counter").text(counter);
 });
+$("#add2").click(function(){
+  //Counter
+  counter = counter.add(2);
+	// Display total
+	$("#counter").text(counter);
+});
+$("#add3").click(function(){
+  //Counter
+  counter = counter.add(3);
+	// Display total
+	$("#counter").text(counter);
+});
+$("#add4").click(function(){
+  //Counter
+  counter = counter.add(4);
+	// Display total
+	$("#counter").text(counter);
+});
+$("#add5").click(function(){
+  //Counter
+  counter = counter.add(5);
+	// Display total
+	$("#counter").text(counter);
+});
+$("#add6").click(function(){
+  //Counter
+  counter = counter.add(6);
+	// Display total
+	$("#counter").text(counter);
+});
+$("#add7").click(function(){
+  //Counter
+  counter = counter.add(7);
+	// Display total
+	$("#counter").text(counter);
+});
+$("#add8").click(function(){
+  //Counter
+  counter = counter.add(8);
+	// Display total
+	$("#counter").text(counter);
+});
+$("#add9").click(function(){
+  //Counter
+  counter = counter.add(9);
+	// Display total
+	$("#counter").text(counter);
+});
+$("#add10").click(function(){
+  //Counter
+  counter = counter.add(10);
+	// Display total
+	$("#counter").text(counter);
+});
 $("#mult2").click(function(){
-  //Add 10 to counter
+  //Counter
   counter = counter.mul(2);
 	// Display total
 	$("#counter").text(counter);
 });
 $("#mult3").click(function(){
-  //Add 10 to counter
+  //Counter
   counter = counter.mul(3);
 	// Display total
 	$("#counter").text(counter);
 });
 $("#mult4").click(function(){
-  //Add 10 to counter
+  //Counter
   counter = counter.mul(4);
 	// Display total
 	$("#counter").text(counter);
 });
 $("#mult5").click(function(){
-  //Add 10 to counter
+  //Counter
   counter = counter.mul(5);
 	// Display total
 	$("#counter").text(counter);
@@ -67,14 +121,14 @@ $("#mult10").click(function(){
 });
 
 //Subtract
-$("#subtract").click(function(){
-  counter = counter - 10;
+$("#sub10").click(function(){
+  counter = counter.sub(10);
   $("#counter").text(counter);
 });
 
 
 // Reset
 $("#reset").click(function(){
-  counter = OmegaNum(2);
+  counter = OmegaNum(0);
   $("#counter").text(counter);
 });
